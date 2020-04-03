a4cebe4950182d451b052b8bea477185c89b0a1b
Austin Hedeen <austin.hedeen@gmail.com>
2020-04-03 16:46:08 -0700

    Update login and register pages with reactivity, dispatches, and links

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { userActions } from '../actions';
+import LoadingButton from "./LoadingButton";
 
 export class LoginPage extends Component {
     constructor(props) {
         super(props);
 
         // reset login status
+        this.props.logout();
 
         this.state = {
             username: '',
@@ -22,34 +24,57 @@ export class LoginPage extends Component {
     }
 
     handleChange(e) {
+        const {name, value} = e.target;
+        this.setState({[name]: value});
     }
 
     handleSubmit(e) {
-        
+        e.preventDefault();
+
+        const { login } = this.props;
+        const { username, password } = this.state;
+
+        this.setState({submitted: true});
+
+        if (username && password) {
+            login(username, password);
+        }
     }
 
     render() {
+        const { authentication } = this.props;
         const { username, password, submitted } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login</h2>
-                <form name="form">
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input
+                            type="text"
+                            className="form-control username"
+                            name="username"
+                            onChange={this.handleChange}
+                        />
                         {submitted && !username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            onChange={this.handleChange}
+                        />
                         {submitted && !password &&
                             <div className="help-block">Password is required</div>
                         }
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
+                        <LoadingButton text="Login" isLoading={Boolean(authentication.loggingIn)} />
+                        <Link to="/register" className="btn btn-link">Register</Link>
                     </div>
                 </form>
             </div>
@@ -58,7 +83,19 @@ export class LoginPage extends Component {
 }
 
 function mapStateToProps(state) {
-
+    const { authentication } = state;
+    return {
+        authentication,
+    }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        login: (username, password) => userActions.login(username, password)(dispatch),
+        logout: () => dispatch(userActions.logout()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
 
 export { LoginPage as TestLoginPage };
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { userActions } from '../actions';
+import LoadingButton from "./LoadingButton";
 
 export class RegisterPage extends Component {
     constructor(props) {
@@ -22,35 +23,61 @@ export class RegisterPage extends Component {
     }
 
     handleChange(event) {
-        // handle input change and dispatch register
+        const {name, value} = event.target;
+
+        this.setState((prevState) => {
+            const user = prevState.user;
+            user[name] = value;
+            return {user};
+        });
     }
 
     handleSubmit(event) {
-        // handle button click and dispatch register
+        event.preventDefault();
+
+        const { register } = this.props;
+        const { user } = this.state;
+
+        this.setState({submitted: true});
+
+        if (user.username && user.password) {
+            register(user);
+        }
     }
 
     render() {
         const { user, submitted } = this.state;
+        const { registration } = this.props;
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register</h2>
-                <form name="form">
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input
+                            type="text"
+                            className="form-control username"
+                            name="username"
+                            onChange={this.handleChange}
+                        />
                         {submitted && !user.username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            onChange={this.handleChange}
+                        />
                         {submitted && !user.password &&
                             <div className="help-block">Password is required</div>
                         }
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
+                        <LoadingButton text="Register" isLoading={Boolean(registration.registering)} />
                         <Link to="/login" className="btn btn-link">Cancel</Link>
                     </div>
                 </form>
@@ -61,7 +88,18 @@ export class RegisterPage extends Component {
 
 // complete the below function
 function mapStateToProps(state) {
-    
+    const { registration } = state;
+    return {
+        registration,
+    }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        register: (user) => userActions.register(user)(dispatch),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterPage);
 
 export { RegisterPage as TestRegisterPage };
