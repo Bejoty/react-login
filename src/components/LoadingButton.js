0ca5a7cc1ec601c68d392e4c3009c0493c52717d
Austin Hedeen <austin.hedeen@gmail.com>
2020-04-03 16:32:29 -0700

    Implement button component for async form submissions

---
+++ src/components/LoadingButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LoadingButton = ({ text, isLoading }) => {
+    return (
+        <button
+            className={`btn btn-${isLoading ? 'secondary' : 'primary'}`}
+            disabled={isLoading}
+        >
+            {isLoading ? 'Loading...' : text}
+        </button>
+    )
+};
+
+LoadingButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+};
+
+export default LoadingButton;
