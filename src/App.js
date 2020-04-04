fc20d1b127591a08d9eb37bd483e79bf36c34031
Austin Hedeen <austin.hedeen@gmail.com>
2020-04-03 17:46:46 -0700

    Setup application routing and alert display functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { PrivateRoute } from './PrivateRoute.js';
+
 import { history } from './helpers';
 import { alertActions } from './actions';
+
 import { HomePage } from './components/HomePage';
-import { LoginPage } from './components/LoginPage';
-import { RegisterPage } from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+
+import AlertBanner from './components/AlertBanner';
 
 export class App extends React.Component {
     constructor(props) {
         super(props);
 
-        const { dispatch } = this.props;
+        const { alertClear } = this.props;
         history.listen((location, action) => {
+            const keepAlert = location.pathname === '/login' &&
+                !!location.state && location.state.status === 'Registration successful';
+            if (!keepAlert) {
+                alertClear();
+            }
         });
     }
 
     render() {
-        const { alert } = this.props;
         return (
               <div className="container">
                   <div className="col-sm-8 col-sm-offset-2">
-                              <LoginPage />
+                      <AlertBanner />
+                      <Router history={history}>
+                          <Switch>
+                              <Route path="/login" component={LoginPage} />
+                              <Route path="/register" component={RegisterPage} />
+                              <PrivateRoute component={HomePage} />
+                          </Switch>
+                      </Router>
                   </div>
               </div>
         );
     }
 }
 
-function mapStateToProps(state) {
-    const { alert } = state;
+function mapDispatchToProps(dispatch) {
     return {
-        alert
-    };
+        alertClear: () => dispatch(alertActions.clear()),
+    }
 }
+
+export default connect(null, mapDispatchToProps)(App);
